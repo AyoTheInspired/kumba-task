73f0a04133bf4b7c77568eb9549f344fff544ad6
Ayo Abimbola <ayoinspired@gmail.com>
2021-09-09 01:35:47 +0100

    App is done

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -20,7 +20,7 @@ function Summary({ items }) {
 		<Wrap className="p-4 col-lg-6 col-md-8 col-sm-10">
 			<h4 className="header mb-3 text-center">Summary</h4>
 			<div className="flex-btw mx-auto p-2">
-				<p className="mb-0 total__tag">Total Items: </p>
+				<p className="mb-0 total__tag">Total Items Purchased: </p>
 
 				<h5 className="total__value mb-0"> {items.length} </h5>
 			</div>
@@ -45,4 +45,9 @@ export default Summary;
 const Wrap = styled.div`
 	background: #fff;
 	box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.8);
+
+	.total__tag {
+		font-weight: bold;
+		font-size: 20px;
+	}
 `;
