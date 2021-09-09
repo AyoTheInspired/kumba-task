6624c52f0a624fe794702ca1f54a6395ab5f7d8d
Ayo Abimbola <ayoinspired@gmail.com>
2021-09-09 02:31:35 +0100

    App is now ready for production

--- src/data-layer/reducer.js
+++ src/data-layer/reducer.js
@@ -1,14 +1,9 @@
 export const initialState = {
 	restaurantData: null,
-	loading: false,
-	isError: false,
-	errorMsg: null,
 };
 
 export const actionTypes = {
 	GET_DATA: "GET_DATA",
-	SET_LOADING: "SET_LOADING",
-	IS_ERROR: "IS_ERROR",
 };
 
 const reducer = (state, action) => {
@@ -16,15 +11,7 @@ const reducer = (state, action) => {
 		case actionTypes.GET_DATA:
 			return {
 				...state,
-				loading: false,
 				restaurantData: action.payload,
-			};
-
-		case actionTypes.IS_ERROR:
-			return {
-				...state,
-				isError: true,
-				errorMsg: action.error,
 			};
 
 		default:
